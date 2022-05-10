409e1c0f6897e0f6a76757b02cf476df6b3c5b51
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-10 10:52:44 -0300

    Controllers v1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,3 +2,19 @@ import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
 import db from "../db.js";
+
+export const postUsers = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const passwordHash = bcrypt.hashSync(password, 10);
+        await db.collection("users").insertOne({
+            name,
+            email,
+            password: passwordHash,
+            itens: []
+        });
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+}
--- src/controllers/itensController.js
+++ src/controllers/itensController.js
@@ -1,1 +1,32 @@
 import db from "../db.js";
+
+export const getItens = async (req, res) => {
+    try {
+        const { user } = res.locals;
+        res.status(201).send(user.itens);
+    } catch {
+        res.sendStatus(500);
+    }
+}
+
+export const postItens = async (req, res) => {
+    try {
+        const { value, description, type, date } = req.body;
+        const { user } = res.locals;
+        const { itens } = user;
+        await db.collection("users").updateOne(user, {
+            $push: {
+                itens: {
+                    value: Number(value),
+                    description,
+                    type,
+                    date,
+                    id: itens.length ? Number(itens[itens.length - 1].id) + 1 : 0
+                }
+            }
+        })
+        res.sendStatus(201);
+    } catch {
+        res.sendStatus(500);
+    }
+}
